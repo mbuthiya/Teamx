3a815d545dc4fd233bf69438334e22f240af39a6
James Muriuki <jamesmuriukimbuthia@gmail.com>
2019-02-17 22:59:14 +0000

    Add basic firebase authentication

---
+++ javascript/main.js
@@ -0,0 +1,21 @@
+// Firebase Setup
+
+var config = {
+    apiKey: "AIzaSyDqap8oXezIx1xZQw1SEVYahE3mnNskcww",
+    authDomain: "teamx-47bae.firebaseapp.com",
+    databaseURL: "https://teamx-47bae.firebaseio.com",
+    projectId: "teamx-47bae",
+    storageBucket: "",
+    messagingSenderId: "899088729893"
+};
+
+firebase.initializeApp(config)
+
+// Database reference
+var database = firebase.database()
+
+// User Interface logic
+$(document).ready(function(){
+
+
+})
